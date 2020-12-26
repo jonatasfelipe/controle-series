93445e5416ba4b1cbe432c126b3a879f27cdb7ac
Jônatas Felipe Santos Gonçalves <jonatas.felipe@hotmail.com.br>
2020-12-26 19:08:23 -0300

    Projeto em Laravel para controle de séries

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/8d5ce0ecc2.js" crossorigin="anonymous"></script>
+    <title>Controle de Séries</title>
+</head>
+<body>
+    <div class="container">
+
+        <div class="jumbotron">
+            <h1>@yield('cabecalho')</h1>
+        </div>
+
+        @yield('conteudo')
+    </div>
+</body>
+</html>
